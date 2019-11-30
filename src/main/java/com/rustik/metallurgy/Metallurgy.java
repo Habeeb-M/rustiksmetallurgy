d79628fd623630713e8597e055844f973f1e5b18
Habeeb-M <habeebmohammed2005@gmail.com>
2019-11-30 12:05:10 +0000

    Initial commit

---
+++ src/main/java/com/rustik/metallurgy/Metallurgy.java
@@ -0,0 +1,8 @@
+package com.rustik.metallurgy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Metallurgy.MODID)
+public class Metallurgy {
+    public static final String MODID = "examplemod";
+}
